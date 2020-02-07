917a5edf7ed9d446a965ea57ae3df6ecebb3f9c2
Robin Edmunds <r.edmunds@live.co.uk>
2020-02-07 10:57:32 +0000

    Change attempt ln fix symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -2,9 +2,9 @@
 
 # symlink all files to current user home and root home
 
-ln -sf -t ~/ .bash_aliases
-ln -sf -t ~/ .vimrc
-ln -sf -t ~/ .tmux.conf
+ln -sf -t ~/ ./.bash_aliases
+ln -sf -t ~/ ./.vimrc
+ln -sf -t ~/ ./.tmux.conf
 
 # ln -sf -t /root/ .bash_aliases
 # ln -sf -t /root/ .vimrc
