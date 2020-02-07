2425276ea74bb08fdd0472b638515e92599a07f5
Robin Edmunds <r.edmunds@live.co.uk>
2020-02-07 11:02:15 +0000

    Fix ln cmds symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -2,10 +2,10 @@
 
 # symlink all files to current user home and root home
 
-ln -sf -t ~/ ./.bash_aliases
-ln -sf -t ~/ ./.vimrc
-ln -sf -t ~/ ./.tmux.conf
+ln -sf -t ~/ $(pwd)/.bash_aliases
+ln -sf -t ~/ $(pwd)/.vimrc
+ln -sf -t ~/ $(pwd)/.tmux.conf
 
-# ln -sf -t /root/ .bash_aliases
-# ln -sf -t /root/ .vimrc
-# ln -sf -t /root/ .tmux.conf
+ln -sf -t /root/ $(pwd)/.bash_aliases
+ln -sf -t /root/ $(pwd)/.vimrc
+ln -sf -t /root/ $(pwd)/.tmux.conf
